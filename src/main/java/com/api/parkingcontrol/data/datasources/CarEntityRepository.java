400811dc1094c328110920672dc9e1dee326ed99
Davi Lima | Software Engineer at Itaú Unibanco <42526185+DaviLima99@users.noreply.github.com>
2023-09-12 14:22:04 -0300

    feat: adding CarEntityRepo

--- src/main/java/com/api/parkingcontrol/data/datasources/CarEntityRepository.java
+++ src/main/java/com/api/parkingcontrol/data/datasources/CarEntityRepository.java
@@ -1,9 +1,7 @@
 package com.api.parkingcontrol.data.datasources;
 
 import com.api.parkingcontrol.data.entity.CarEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
 public interface CarEntityRepository extends CrudRepository<CarEntity, String> {
 }
